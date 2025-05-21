e673a1f13bf633b5ed9123bed0e76c3df3871c3a
Your Name <you@example.com>
2025-05-21 07:02:55 +0000

    some fixes

--- fingerprint/fingerprint/doctype/fingerprint_config/fingerprint_config.py
+++ fingerprint/fingerprint/doctype/fingerprint_config/fingerprint_config.py
@@ -49,13 +49,12 @@ fing_config = {
     'ERPNEXT_VERSION': 14,
     'IMPORT_START_DATE':None,
     'LOGS_DIRECTORY': 'logs',
-    'devices': None
 }
 
 config = dotdict(fing_config)
 
-device_punch_values_IN = getattr(fing_config, 'device_punch_values_IN', [0,4])
-device_punch_values_OUT = getattr(fing_config, 'device_punch_values_OUT', [1,5])
+device_punch_values_IN = getattr(fing_config, 'device_punch_values_IN', [1,4])
+device_punch_values_OUT = getattr(fing_config, 'device_punch_values_OUT', [0,5])
 ERPNEXT_VERSION = getattr(fing_config, 'ERPNEXT_VERSION', 14)
 
 if not os.path.exists(config.LOGS_DIRECTORY):
@@ -67,18 +66,14 @@ status = PickleDB('/'.join([config.LOGS_DIRECTORY, 'status.json']))
 
 def main():
     try:
-        for device_num, device in enumerate(config.devices):
-            # device = config.devices[0]
-            device_attendance_logs = None
-            info_logger.info("Processing Device: "+ device['device_id'])
+        for file_name in os.listdir('/home/frappe/frappe-bench/sites/frontend/private/files'):
+            file_path = os.path.join('/home/frappe/frappe-bench/sites/frontend/private/files', file_name)
+            info_logger.info("Processing File: "+ file_path)
             try:
-                frappe.publish_progress(device_num/len(config.devices)*100, title=f"Fetching data from fingerprint device {device_num+1}...", description = "please wait until fetching data" )
-                pull_process_and_push_data(device, device_attendance_logs)
-                frappe.publish_progress((device_num+1)/len(config.devices)*100, title=f"Fetching data from fingerprint {device_num+1} device is finished", description = "done" )
-
-                info_logger.info("Successfully processed Device: "+ device['device_id'])
+                pull_process_and_push_data(file_path)
+                info_logger.info("Successfully processed File: "+ file_path)
             except:
-                error_logger.exception('exception when calling pull_process_and_push_data function for device'+json.dumps(device, default=str))
+                error_logger.exception('exception when calling pull_process_and_push_data function for file '+ file_path)
             info_logger.info("Mission Accomplished!")
     except:
         error_logger.exception('exception has occurred in the main function...')
@@ -110,7 +105,11 @@ def sort_file_by_timestamp(file_path):
         for _, line in parsed_lines:
             file.write(line)
 
-def pull_process_and_push_data(device, device_attendance_logs=None):
+def edit_attendance(record):
+    record['timestamp'] = datetime.datetime.fromtimestamp(record['timestamp'])
+    return record
+
+def pull_process_and_push_data(file_path):
     
     """ Takes a single device config as param and pulls data from that device.
 
@@ -118,128 +117,40 @@ def pull_process_and_push_data(device, device_attendance_logs=None):
     device: a single device config object from the local_config file
     device_attendance_logs: fetching from device is skipped if this param is passed. used to restart failed fetches from previous runs.
     """
-    attendance_success_log_file = '_'.join(["attendance_success_log", device['device_id']])
-    attendance_failed_log_file = '_'.join(["attendance_failed_log", device['device_id']])
-    attendance_success_logger = setup_logger(attendance_success_log_file, '/'.join(['logs', attendance_success_log_file])+'.log')
-    attendance_failed_logger = setup_logger(attendance_failed_log_file, '/'.join(['logs', attendance_failed_log_file])+'.log')
+
+    content = open(file_path, 'r').read()
+    attendances = json.loads(content)
+    updated_attendances = [edit_attendance(att) for att in attendances]
+    attendances = sorted(updated_attendances, key=lambda x: x['timestamp'])
+
+    device_attendance_logs = attendances
     if not device_attendance_logs:
-        device_attendance_logs = get_all_attendance_from_device(device['ip'], device_id=device['device_id'], clear_from_device_on_fetch=device['clear_from_device_on_fetch'])
-        if not device_attendance_logs:
             return
     import_start_date = _safe_convert_date(config.IMPORT_START_DATE, "%Y%m%d")
+    
     for i, x in enumerate(device_attendance_logs):
-            if x['timestamp'] >= import_start_date:
-                index_of_last = i
-                break
-        # Process each log in the determined range
-    for device_attendance_log in device_attendance_logs[index_of_last+1:]:
-        punch_direction = device['punch_direction']
-        if punch_direction == 'AUTO':
-            if device_attendance_log['punch'] in device_punch_values_OUT:
-                punch_direction = 'OUT'
-            elif device_attendance_log['punch'] in device_punch_values_IN:
-                punch_direction = 'IN'
-            else:
-                punch_direction = None
+        if x['timestamp'] >= import_start_date:
+            index_of_last = i
+            break
+
+    # Process each log in the determined range
+    for device_attendance_log in device_attendance_logs[index_of_last:]:
+        if device_attendance_log['punch'] in device_punch_values_OUT:
+            punch_direction = 'OUT'
+        elif device_attendance_log['punch'] in device_punch_values_IN:
+            punch_direction = 'IN'
+        else:
+            punch_direction = None
 
         try:
             add_log_based_on_employee_field(
-                device_attendance_log['user_id'], device_attendance_log['timestamp'], device['device_id'], punch_direction
+                device_attendance_log['user_id'], device_attendance_log['timestamp'], "device_id", punch_direction
             )
 
         except Exception as e:
             # error_logger.exception(f"Error fetching user {device_attendance_log['user_id']}: {e}")
             pass
 
-
-
-def get_all_attendance_from_device(ip, port=4370, timeout=30, device_id=None, clear_from_device_on_fetch=False):
-    
-    zk = ZK(ip, port=port, timeout=timeout)
-    conn = None
-    attendances = []
-    try:
-        conn = zk.connect()
-        x = conn.disable_device()
-        # device is disabled when fetching data
-        info_logger.info("\t".join((ip, "Device Disable Attempted. Result:", str(x))))
-        attendances = conn.get_attendance()
-        info_logger.info("\t".join((ip, "Attendances Fetched:", str(len(attendances)))))
-        status.set(f'{device_id}_push_timestamp', None)
-        status.set(f'{device_id}_pull_timestamp', str(datetime.datetime.now()))
-        status.save()
-        if len(attendances):
-            # keeping a backup before clearing data incase the programs fails.
-            # if everything goes well then this file is removed automatically at the end.
-            dump_file_name = get_dump_file_name_and_directory(device_id, ip)
-
-            with open(dump_file_name, 'w+') as f:
-                f.write(json.dumps(list(map(lambda x: x.__dict__, attendances)), default=datetime.datetime.timestamp))
-        x = conn.enable_device()
-        info_logger.info("\t".join((ip, "Device Enable Attempted. Result:", str(x))))
-    except:
-        error_logger.exception(str(ip)+' exception when fetching from device...')
-        raise Exception('Device fetch failed.')
-    finally:
-        if conn:
-            conn.disconnect()
-    return list(map(lambda x: x.__dict__, attendances))
-
-
-
-def update_shift_last_sync_timestamp(shift_type_device_mapping):
-    
-    """
-    ### algo for updating the sync_current_timestamp
-    - get a list of devices to check
-    - check if all the devices have a non 'None' push_timestamp
-        - check if the earliest of the pull timestamp is greater than sync_current_timestamp for each shift name
-            - then update this min of pull timestamp to the shift
-
-    """
-    for shift_type_device_map in shift_type_device_mapping:
-        all_devices_pushed = True
-        pull_timestamp_array = []
-        for device_id in shift_type_device_map['related_device_id']:
-            if not status.get(f'{device_id}_push_timestamp'):
-                all_devices_pushed = False
-                break
-            pull_timestamp_array.append(_safe_convert_date(status.get(f'{device_id}_pull_timestamp'), "%Y-%m-%d %H:%M:%S.%f"))
-        if all_devices_pushed:
-            min_pull_timestamp = min(pull_timestamp_array)
-            if isinstance(shift_type_device_map['shift_type_name'], str): # for backward compatibility of config file
-                shift_type_device_map['shift_type_name'] = [shift_type_device_map['shift_type_name']]
-            for shift in shift_type_device_map['shift_type_name']:
-                try:
-                    sync_current_timestamp = _safe_convert_date(status.get(f'{shift}_sync_timestamp'), "%Y-%m-%d %H:%M:%S.%f")
-                    if (sync_current_timestamp and min_pull_timestamp > sync_current_timestamp) or (min_pull_timestamp and not sync_current_timestamp):
-                        response_code = send_shift_sync_to_erpnext(shift, min_pull_timestamp)
-                        if response_code == 200:
-                            status.set(f'{shift}_sync_timestamp', str(min_pull_timestamp))
-                            status.save()
-                except:
-                    error_logger.exception('Exception in update_shift_last_sync_timestamp, for shift:'+shift)
-
-def send_shift_sync_to_erpnext(shift_type_name, sync_timestamp):
-    
-    url = config.ERPNEXT_URL + "/api/resource/Shift Type/" + shift_type_name
-    headers = {
-        'Authorization': "token "+ config.ERPNEXT_API_KEY + ":" + config.ERPNEXT_API_SECRET,
-        'Accept': 'application/json'
-    }
-    data = {
-        "last_sync_of_checkin" : str(sync_timestamp)
-    }
-    try:
-        response = requests.request("PUT", url, headers=headers, data=json.dumps(data))
-        if response.status_code == 200:
-            info_logger.info("\t".join(['Shift Type last_sync_of_checkin Updated', str(shift_type_name), str(sync_timestamp.timestamp())]))
-        else:
-            error_str = _safe_get_error_str(response)
-            error_logger.error('\t'.join(['Error during ERPNext Shift Type API Call.', str(shift_type_name), str(sync_timestamp.timestamp()), error_str]))
-        return response.status_code
-    except:
-        error_logger.exception("\t".join(['exception when updating last_sync_of_checkin in Shift Type', str(shift_type_name), str(sync_timestamp.timestamp())]))
 
 def get_last_line_from_file(file):
     
@@ -293,52 +204,42 @@ def _safe_get_error_str(res):
         error_str = str(res.__dict__)
     return error_str
 
-def generate_keys(user):
-
-    user_details = frappe.get_doc('User', user)
-    api_secret = frappe.generate_hash(length=15)
-
-    if not user_details.api_key:
-        api_key = frappe.generate_hash(length=15)
-        user_details.api_key = api_key
-
-    frappe.db.set_value('User', user, 'api_secret', api_secret)
-
-    return api_secret
-
-
 
 class fingerprint_config(Document):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)  
-        if self.get('device_ip_list'):
-            device_IP_list = self.get('device_ip_list').split(",")
-            device_IP_list = [item.strip() for item in device_IP_list]
+        # if self.get('device_ip_list'):
+        #     device_IP_list = self.get('device_ip_list').split(",")
+        #     device_IP_list = [item.strip() for item in device_IP_list]
 
-            device_ID_list = self.get('device_id_list').split(",")
-            device_ID_list = [item.strip() for item in device_ID_list]
+        #     device_ID_list = self.get('device_id_list').split(",")
+        #     device_ID_list = [item.strip() for item in device_ID_list]
         
-            self.devices = []
-            for device_ip, device_id in zip(device_IP_list, device_ID_list):
-                self.devices.append({'device_id':device_id,'ip':device_ip, 'punch_direction': 'AUTO', 'clear_from_device_on_fetch': False})
+        #     self.devices = []
+        #     for device_ip, device_id in zip(device_IP_list, device_ID_list):
+        #         self.devices.append({'device_id':device_id,'ip':device_ip, 'punch_direction': 'AUTO', 'clear_from_device_on_fetch': False})
             
-            ERPNEXT_VERSION = 14
-            IMPORT_START_DATE = self.get('import_start_date')
-            LOGS_DIRECTORY = 'logs' # logs of this script is stored in this directory
+        ERPNEXT_VERSION = 14
+        IMPORT_START_DATE = self.get('import_start_date')
+        if IMPORT_START_DATE:
+            IMPORT_START_DATE = IMPORT_START_DATE.replace('-','')
+            
+        LOGS_DIRECTORY = 'logs' # logs of this script is stored in this directory
 
 
-            fing_config = {
-                'ERPNEXT_VERSION': ERPNEXT_VERSION,
-                'IMPORT_START_DATE':IMPORT_START_DATE,
-                'LOGS_DIRECTORY': LOGS_DIRECTORY,
-                'devices': self.devices
-            }
+        fing_config = {
+            'ERPNEXT_VERSION': ERPNEXT_VERSION,
+            'IMPORT_START_DATE':IMPORT_START_DATE,
+            'LOGS_DIRECTORY': LOGS_DIRECTORY,
+        }
 
-            global config
-            config = dotdict(fing_config)
+        global config
+        config = dotdict(fing_config)
 
     def get(self, key):
         return self.__dict__.get(key, None)
 
     def before_save(self):	
+        info_logger.info(str(config))
+        
         main()
